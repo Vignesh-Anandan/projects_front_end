31ed681a8b8625b50d33e3b6f750b02b0537b85d
Vignesh-Anandan <vignesh.a0695@gmail.com>
2024-12-21 20:15:11 +0530

    githuntrail

---
+++ App.js
@@ -0,0 +1,16 @@
+
+const parent = React.createElement("div",{id: "parent"},
+    React.createElement("div",{id: "child"},
+    React.createElement("h1",{}, "i am h1")
+)
+);
+
+console.log(parent);
+
+
+const heading = React.createElement
+("h1", 
+    {id : "heading"}, 
+    "You will be Developer soon");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
